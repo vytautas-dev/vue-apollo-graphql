fd940d005013e36b6913fa41aff81a9d035d4387
Wojciech Szczygielski <wojciech.szczygielski91@gmail.com>
2022-11-18 14:20:48 +0100

    google auth added

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,34 @@
 <!-------------------------------------------------SCRIPT---------------------------------------------------->
 <script setup lang="ts">
+import {userAuthStore} from "../stores/userAuthStore";
+
+const userStore = userAuthStore();
+
+const redirectToGoogleSSO = async () => {
+  let timer: NodeJS.Timeout | null = null;
+  const googleLoginURL = "http://localhost:5000/auth/google";
+  const newWindow = window.open(
+      googleLoginURL,
+      "_blank",
+      "width=500,height=600"
+  );
+  if (newWindow) {
+    timer = setInterval(() => {
+      if (newWindow.closed) {
+        userStore.fetchUser();
+        if (timer) clearInterval(timer);
+      }
+    }, 500);
+  }
+};
 
 </script>
 
 <!-------------------------------------------------TEMPLATE-------------------------------------------------->
 <template>
   <h2>Login Page</h2>
+  <button @click="redirectToGoogleSSO" v-if="!userStore.user">LOGIN WITH GOOGLE</button>
+  <div v-else><p>{{userStore.user.username}}</p></div>
 </template>
 
 <!-------------------------------------------------STYLE----------------------------------------------------->
